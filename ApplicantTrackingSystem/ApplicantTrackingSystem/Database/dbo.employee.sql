c8e655913c7f50c02a7f1c89478390a492511394
RichardC0M <59419372+RichardC0M@users.noreply.github.com>
2020-12-03 21:42:51 +0000

    Disabled overwriting of runtime database, increased maximum length for several attributes.

--- ApplicantTrackingSystem/ApplicantTrackingSystem/Database/dbo.employee.sql
+++ ApplicantTrackingSystem/ApplicantTrackingSystem/Database/dbo.employee.sql
@@ -1,9 +1,10 @@
 ﻿CREATE TABLE [dbo].[employee] (
     [employee_id]   INT           IDENTITY (1, 1) NOT NULL,
     [user_id]       INT           NOT NULL,
-    [job_title]     VARCHAR (25)  NOT NULL,
+    [job_title]     VARCHAR (50)  NOT NULL,
     [password]      VARCHAR (256) NOT NULL,
     [administrator] BIT           NOT NULL,
-    PRIMARY KEY CLUSTERED ([employee_id] ASC), 
-    CONSTRAINT [FK_employee_ToTable] FOREIGN KEY ([user_id]) REFERENCES [dbo].[users]([user_id])
+    PRIMARY KEY CLUSTERED ([employee_id] ASC),
+    CONSTRAINT [FK_employee_ToTable] FOREIGN KEY ([user_id]) REFERENCES [dbo].[users] ([user_id]) ON DELETE CASCADE
 );
+
--- ApplicantTrackingSystem/ApplicantTrackingSystem/Database/dbo.template.sql
+++ ApplicantTrackingSystem/ApplicantTrackingSystem/Database/dbo.template.sql
@@ -1,7 +1,8 @@
 ﻿CREATE TABLE [dbo].[template] (
     [template_id] INT          IDENTITY (1, 1) NOT NULL,
-    [title]       VARCHAR (25) NOT NULL,
+    [title]       VARCHAR (50) NOT NULL,
     [header]      TEXT         NOT NULL,
     [footer]      TEXT         NOT NULL,
     PRIMARY KEY CLUSTERED ([template_id] ASC)
 );
+
